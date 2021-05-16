a77d519cbb319ebd8ddfacf04b266d01705c5086
Vu Do <57383837+VuDo314@users.noreply.github.com>
2021-05-15 22:53:31 -0700

    Add fancifymytext.js for fancifymytext.html

---
+++ fancifymytext.js
@@ -0,0 +1,30 @@
+function makeTextBigger() {   
+   document.getElementById("textInput").style.fontSize = "24pt";
+}
+   
+function makeTextBoring() {
+    if (document.getElementById("boring").checked == true) {    
+        document.getElementById("textInput").style.fontSize = "10pt";
+        document.getElementById("textInput").style.color = "black";
+        document.getElementById("textInput").style.textDecoration = "none";
+        document.getElementById("boring").checked = false;
+    }
+}
+
+function makeTextFancy() {
+    if (document.getElementById("fancy").checked == true) {
+        document.getElementById("textInput").style.color = "blue";
+        document.getElementById("textInput").style.textDecoration = "underline";
+        document.getElementById("fancy").checked = false;
+    }
+}
+
+function Click() {
+    var Click = document.getElementById("textInput");
+    Click.value = Click.value.toUpperCase();
+    var parts = Click.value.split(".");
+    for (var i = 0; i< parts.length; i++){
+        parts[i] = parts[i] + "Click";
+    }
+    Click.value = parts.join(".");
+}
