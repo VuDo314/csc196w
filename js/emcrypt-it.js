ed362b5ce6be7d54aa95b62c8a4ace3e429bae69
Vu Do <57383837+VuDo314@users.noreply.github.com>
2021-05-16 17:07:06 -0700

    Update emcrypt-it.js

--- js/emcrypt-it.js
+++ js/emcrypt-it.js
@@ -6,7 +6,6 @@
    * for the "load" event on the window, signalling the HTML DOM has been constructed
    * on the page. When this event occurs, the attached function (init) will be called.
    */
-   
   window.addEventListener("load", init);
 
   /**
@@ -15,30 +14,21 @@
   function init() {
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
-	
     const encryptIt = document.getElementById('encrypt-it')
     encryptIt.addEventListener("click", click, false);
-	
   }
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
-  function handleClick() {
-	  
+  function click() {
     console.log("Button clicked!");
     const inputText = document.getElementById('input-text');
     const resultArea = document.getElementById('result');
     resultArea.textContent = shiftCipher(inputText.value);
-	
   }
 
-  /**
- * Returns an encrypted version of the given text, where
- * each letter is shifted alphabetically ahead by 1 letter,
- * and 'z' is shifted to 'a' (creating an alphabetical cycle).
- */
-function shiftCipher(text) {
+  function shiftCipher(text) {
     text = text.toLowerCase();
     const upgrade = 2;
     let result = '';
@@ -46,15 +36,13 @@ function shiftCipher(text) {
       if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
         if (text[i] == 'y') {
           result += String.fromCharCode(97);
-        }
-	else if (text[i] == 'z') {
+        }else if (text[i] == 'z') {
           result += String.fromCharCode(98);
-        } 
-	else {
+        } else {
           result += String.fromCharCode(text.charCodeAt(i) + upgrade);
+  
         }
-      } 
-      else {
+      } else {
         result += text[i];
       }
       
