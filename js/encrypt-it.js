2ac68976ca549393d1b63d6abc859dc20535a5f2
Vu Do <57383837+VuDo314@users.noreply.github.com>
2021-05-16 17:39:56 -0700

    Update encrypt-it.js

--- js/encrypt-it.js
+++ js/encrypt-it.js
@@ -6,6 +6,7 @@
    * for the "load" event on the window, signalling the HTML DOM has been constructed
    * on the page. When this event occurs, the attached function (init) will be called.
    */
+   
   window.addEventListener("load", init);
 
   /**
@@ -14,23 +15,26 @@
   function init() {
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
+	
     const encryptIt = document.getElementById('encrypt-it')
     encryptIt.addEventListener("click", click, false);
+	
   }
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
-  function click() {
+  function handleClick() {
+	  
     console.log("Button clicked!");
     const inputText = document.getElementById('input-text');
     const resultArea = document.getElementById('result');
     resultArea.textContent = shiftCipher(inputText.value);
+	
   }
-
-   function shiftCipher(text) {
+  function shiftCipher(text) {
     text = text.toLowerCase();
-    const shift = 2;
+    let shift = 2;
     let result = '';
     for (let i = 0; i <text.length; i++) {
       if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
@@ -40,9 +44,12 @@
         result += String.fromCharCode(text.charCodeAt(i) + shift);
       }
       else {
-		    result += text[i];
-	    }		
+		result += text[i];
+	  }		
     }
     return result;
   }
 })();
+
+
+
