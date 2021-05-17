d6d1931e226404e27dc319ce5979edac6e5959db
Vu Do <57383837+VuDo314@users.noreply.github.com>
2021-05-16 17:46:48 -0700

    Update encrypt-it.js

--- js/encrypt-it.js
+++ js/encrypt-it.js
@@ -39,13 +39,13 @@
     for (let i = 0; i <text.length; i++) {
       if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
         if ((text.charCodeAt(i) + shift) > 122) {
-          shift = shift - 122;
+          shift -= 122;
         }
         result += String.fromCharCode(text.charCodeAt(i) + shift);
       }
       else {
-		result += text[i];
-	  }		
+	result += text[i];
+      }		
     }
     return result;
   }
